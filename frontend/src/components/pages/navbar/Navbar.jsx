2e9d372b90adc407ea5e2f70e9b9c95e73137473
Shubham Deshmukh <shubhamdeshm37@gmail.com>
2025-02-08 17:13:19 +0530

    registration form has been developed

--- frontend/src/components/pages/navbar/Navbar.jsx
+++ frontend/src/components/pages/navbar/Navbar.jsx
@@ -5,13 +5,14 @@ import Style from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <div className={Style["nav"]}>
-      <h2>E-Commerce</h2>
+      <h2 style={{color:"black"}}> E-Commerce</h2>
       <ul>
         <li><Link to="/">Products</Link></li>
         <li><Link to="/addproduct">Add Products</Link></li>
         <li><Link to="/update">Update Products</Link></li>
-        <li><Link to="/logout">Logout</Link></li>
+        <li><Link to="/register">Register</Link></li>
         <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/logout">Logout</Link></li>
       </ul>
     </div>
   );
